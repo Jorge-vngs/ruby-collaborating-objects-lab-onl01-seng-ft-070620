54515d088e5f7699bfeafd826b30b34d073cec8d
Jorge Venegas <brangelina01@gmail.com>
2020-07-22 06:01:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,28 +1,27 @@
-class Song
-  attr_accessor :name, :artist
-  @@all = []
-  
-  def self.all
-    @@all
-  end
+class Song	
+    @@all = []	
+    attr_accessor :artist, :name	
 
+    def initialize(name)	
+        @name = name	
+        @@all << self	
+    end	
 
+    def self.all	
+        @@all	
+    end	
 
-  def initialize(name)
-    @name = name
-  end
+    def artist_name=(name)	
+        self.artist = Artist.find_or_create_by_name(name)
+        artist.add_song(self)	
+    end	
 
-  def self.new_by_filename(file)
-    artist_name = file.split(" - ")[0]
-    song_name = file.split(" - ")[1]
-    song = Song.new(song_name)
-    song.artist_name = artist_name
-    song
-  end
+    def self.new_by_filename(filename)	
+        artist = filename.split(" - ")[0]	
+        song = filename.split(" - ")[1]	
+        new_song = Song.new(song)	
+        new_song.artist=(Artist.new(artist))	
+        new_song	
+    end	
 
-  def artist_name=(name)
-    artist = Artist.find_or_create_by_name(name)
-    self.artist = artist
-    artist.add_song(self)
-  end
-end 
+end
