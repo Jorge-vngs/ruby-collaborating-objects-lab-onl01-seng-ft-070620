32595b5354f518c82c589bb8481d0e209572aa9b
Jorge Venegas <brangelina01@gmail.com>
2020-07-22 05:38:47 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,46 +1,38 @@
 class Artist
-  attr_accessor :name, :songs, 
-  @@all = [] 
+  attr_accessor :name, :songs
+  @@all =[]
 
   def initialize(name)
     @name = name
-    @songs = [] 
-  end
-
-  def self.all
-    @@all
+    @songs = []
   end
 
   def add_song(song)
     @songs << song
   end
 
-  def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
-  end
-
-  def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
-
   def save
-    @@all << self
+    @@all << self 
   end
 
-  def print_songs()
-    puts @songs.collect {|x| x.name}
+  def self.all
+    @@all 
   end
 
+  def self.find_or_create_by_name(artist_name)
+    found_artist = self.all.find {|artist| artist.name == artist_name}
+    if found_artist
+      found_artist
+    else
+      new_artist = self.new(artist_name)
+      new_artist.save
+      new_artist
+    end
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
+  end
 end
